5f91b854eefb0861f7521aa85b3d63bd9e2beaeb
Vander Alves <vanderzago@gmail.com>
2020-03-07 13:42:52 -0300

    orquestracao

--- Dockerfile
+++ Dockerfile
@@ -1,18 +1,9 @@
 FROM rundeck/rundeck:SNAPSHOT
 
-ARG USER
-ARG PASS
-ARG URL
-
 LABEL maintainer="vander.alves@inmetrics.com.br"
 
 USER root
 
 COPY rundeck-cli-1.1.8-all.jar /tmp/
-COPY src/ /tmp/
-
-RUN chmod +x /tmp/sh/load_jobs.sh
-
-USER rundeck
-
-#CMD ["/tmp/sh/load_jobs.sh","${USER}","${PASS}","${URL}"]
+COPY src/yaml/ /tmp/
+COPY src/sh/load_jobs.sh /docker-entrypoint.d/
--- src/sh/load_jobs.sh
+++ src/sh/load_jobs.sh
@@ -1,26 +1,29 @@
 #!/bin/bash
-
-USER=$1
-PASS=$2
-URL=$3
-
-export RD_URL=$URL
-export RD_USER=$USER
-export RD_PASSWORD=$PASS
-
-LOOP=0
-RETRIES=10
-while [[ ${LOOP} -eq 0 && $RETRY -lt ${RETRIES} ]]
-do
-    curl --fail $RD_URL; Res=$?
-
-    if [[ $Res -eq 0 ]]
-    then
-        java -jar /tmp/rundeck-cli-1.1.8-all.jar projects create -p Graylog
-        java -jar /tmp/rundeck-cli-1.1.8-all.jar jobs load -p Graylog --file /tmp/yaml/job_graylog_input.yaml -F yaml
-        LOOP=1
-    else
-        sleep 30
-        RETRY=$((RETRY+1))
-    fi
-done
+curl --location --request POST "http://graylog:9000/api/system/inputs" \
+    --header 'Accept: application/json' \
+    --header 'Content-Type: application/json' \
+    --header "Authorization: Basic YWRtaW46YWRtaW4=" \
+    --header 'X-Requested-By: cli' \
+    --data-raw '{
+            "title": "Algamoney-API",
+            "type": "org.graylog2.inputs.gelf.udp.GELFUDPInput",
+            "global": true,
+            "configuration":   {
+                "recv_buffer_size": 1048576,
+                "tcp_keepalive": false,
+                "use_null_delimiter": true,
+                "number_worker_threads": 2,
+                "tls_client_auth_cert_file": "",
+                "bind_address": "0.0.0.0",
+                "tls_cert_file": "",
+                "decompress_size_limit": 8388608,
+                "port": 12201,
+                "tls_key_file": "",
+                "tls_enable": false,
+                "tls_key_password": "",
+                "max_message_size": 2097152,
+                "tls_client_auth": "disabled",
+                "override_source": null
+                },
+            "node": null
+        }'
